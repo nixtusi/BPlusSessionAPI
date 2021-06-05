2786dce94a8f213892af55f7550fa5caf33526c1
nixtusi <yuta.nishimatsu@gmail.com>
2021-06-05 12:58:16 +0900

    nyoxtuki

--- script.py
+++ script.py
@@ -1,27 +1,4 @@
-from flask import Flask, request, abort
-import os
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-app = Flask(__name__)
-
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-
-@app.route("/")
 def hello_world():
     return "hello world!"
 
@@ -32,7 +9,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info("Request body: " + body + "にょっき")
 
     # handle webhook body
     try:
